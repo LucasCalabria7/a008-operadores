59feb88f1dfa8e55a79a4dd46448e8bfe03d9413
Lucas Calabria <lucascalabria86@gmail.com>
2022-08-30 15:32:13 -0300

    Commit final

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -2,9 +2,9 @@ let exercicioA = (5 > 20) && (5 < 2);
 
 let exercicioB = (5 === 5) || (5 === "5");
 
-let exercicioC = !20 > 50;
+let exercicioC = !(20 > 50);
 
-let exercicioD = (!20 > 50) || (50 > 60);
+let exercicioD = !(20 > 50) ||!(50 > 60);
 
 console.log(exercicioA);
 console.log(exercicioB);
