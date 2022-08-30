63db245689cc702bab4cd7b37451862d28f35306
Lucas Calabria <lucascalabria86@gmail.com>
2022-08-30 13:46:35 -0300

    Commitando exercícios de operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let numeroPar = Number(prompt("Insira um número Par"))
+console.log(numeroPar%2);
+
+//Resto da Divisão por 2 de um número par sempre será 0
+//Resto da Divisão por números ímpar sempre vai ser 1
